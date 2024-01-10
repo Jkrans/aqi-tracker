55a394f1af56cd671c010e508d62698179f469c2
Jeremy Krans <49561917+Jkrans@users.noreply.github.com>
2024-01-10 12:31:50 -0700

    setting up railway backend

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+// server.js
+require('dotenv').config();
+const express = require('express');
+
+let fetch;
+import('node-fetch').then(({ default: fetchModule }) => {
+    fetch = fetchModule;
+    // Rest of your server code that uses fetch
+});
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Allow CORS (Cross-Origin Resource Sharing) for your frontend
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+app.get('/aqi', async (req, res) => {
+    // Get city from the query string
+    const city = req.query.city;
+
+    // Construct API URL
+    const apiUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(city)}?contentType=json&unitGroup=metric&key=${process.env.VISUAL_CROSSING_API_KEY}&elements=datetime,aqius`;
+
+    try {
+        const apiResponse = await fetch(apiUrl);
+        const apiData = await apiResponse.json();
+        res.json(apiData.currentConditions.aqius);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -15,22 +15,18 @@ const MainContent = () => {
         setAqi(null);
 
         try {
-            const response = await fetch(
-                `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}
-                ?contentType=json&unitGroup=metric&key=${process.env.REACT_APP_VISUAL_CROSSING_API_KEY}
-                &elements=datetime,aqius`
-            );
+            const response = await fetch(`http://localhost:3001/aqi?city=${encodeURIComponent(city)}`);
             if (!response.ok) {
                 throw new Error('Please try your request again');
             }
-            const data = await response.json();
-            console.log(data)
-            setAqi(data.currentConditions.aqius);
-
+            const aqiValue = await response.json();
+            setAqi(aqiValue);
         } catch (err) {
             setError(err.message);
         }
     };
+
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
