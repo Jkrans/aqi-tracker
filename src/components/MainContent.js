18e2fd8593497a08283bc4c7143926ab02a6b80d
Jeremy Krans <49561917+Jkrans@users.noreply.github.com>
2024-01-10 12:37:27 -0700

    setting up railway backend. take 2

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -5,7 +5,7 @@ const MainContent = () => {
     const [aqi, setAqi] = useState(null);
     const [error, setError] = useState(null);
 
-
+    // test
     const handleInputChange = (e) => {
         setCity(e.target.value);
     };
