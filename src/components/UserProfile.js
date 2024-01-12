aea006f2603ff3b1228aa8a7bff583e313e1f8a5
Jeremy Krans <49561917+Jkrans@users.noreply.github.com>
2024-01-12 11:18:37 -0700

    deleted auth0 app and started over. Working locally now. Going to try in production.

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,34 +1,22 @@
-import { useEffect } from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const UserProfile = () => {
-    const { user, isAuthenticated, isLoading, error, handleRedirectCallback } = useAuth0();
+    const { user, isAuthenticated, isLoading } = useAuth0();
 
-    useEffect(() => {
-        if (window.location.search.includes('code=')) {
-            // If the URL contains the authorization code, handle the callback
-            handleRedirectCallback().then(() => {
-                console.log('Handled redirect callback');
-            }).catch(console.error);
-        }
+    if (isLoading) {
+        return <div>Loading ...</div>;
+    }
 
-        console.log('Authentication Status:', isAuthenticated);
-        console.log('Loading Status:', isLoading);
-        console.log('Error:', error);
-    }, [isAuthenticated, isLoading, error, handleRedirectCallback]);
-
-    if (isAuthenticated) {
-        return (
+    return (
+        isAuthenticated && (
             <div>
                 <img src={user.picture} alt={user.name} />
                 <h2>{user.name}</h2>
-                <p>Email: {user.email}</p>
-                {/* Add other user information you want to display */}
+                <p>{user.email}</p>
             </div>
-        );
-    } else {
-        return <p>User is not logged in.</p>;
-    }
+        )
+    );
 };
 
 export default UserProfile;
--- src/index.js
+++ src/index.js
@@ -9,12 +9,15 @@ const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri="https://aqi-tracker.netlify.app/profile"
+      authorizationParams={{
+        redirect_uri: window.location.origin + "/profile"
+      }}
     >
       <App />
     </Auth0Provider>
