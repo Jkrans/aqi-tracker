6c7ca12e0e249db7a964748d3c3392154bb50e30
Jeremy Krans <49561917+Jkrans@users.noreply.github.com>
2024-01-15 11:37:28 -0700

    Added log to LogoutButton because in production it's trying to redirect to localhost for some reason

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 export const LoginButton = () => {
@@ -12,7 +12,9 @@ export const LogoutButton = () => {
     const { logout } = useAuth0();
     const handleLogout = () => {
         console.log("Current origin:", window.location.origin); // For debugging
-        logout({ returnTo: window.location.origin });
+        setTimeout(() => { logout({ returnTo: window.location.origin }); }, 3000)
+
+
     };
     return <button onClick={handleLogout}>Log Out</button>;
 };
